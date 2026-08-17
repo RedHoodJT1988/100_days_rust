87caaff3bff7c8b32468ae8120f8f78d001a97fc
Jonathan Reeves <jonathanedwardreeves1988@gmail.com>
2026-08-17 12:39:23 -0500

    Complete Day 2: temperature converter program

---
+++ temperature_converter/target/debug/deps/temperature_converter.d
@@ -0,0 +1,5 @@
+H:\rust\100_days_rust\temperature_converter\target\debug\deps\temperature_converter.d: src\main.rs
+
+H:\rust\100_days_rust\temperature_converter\target\debug\deps\temperature_converter.exe: src\main.rs
+
+src\main.rs:
---
+++ temperature_converter/target/debug/temperature_converter.d
@@ -0,0 +1,1 @@
+H:\rust\100_days_rust\temperature_converter\target\debug\temperature_converter.exe: H:\rust\100_days_rust\temperature_converter\src\main.rs
