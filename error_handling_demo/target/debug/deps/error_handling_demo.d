7a07c2ef4b81706c84228f866e2a8745a0b56297
Jonathan Reeves <jonathanedwardreeves1988@gmail.com>
2026-08-22 11:28:05 -0500

    Complete Day 21: Error Handling Demo

---
+++ error_handling_demo/target/debug/deps/error_handling_demo.d
@@ -0,0 +1,5 @@
+H:\rust\100_days_rust\error_handling_demo\target\debug\deps\error_handling_demo.d: src\main.rs
+
+H:\rust\100_days_rust\error_handling_demo\target\debug\deps\error_handling_demo.exe: src\main.rs
+
+src\main.rs:
---
+++ error_handling_demo/target/debug/error_handling_demo.d
@@ -0,0 +1,1 @@
+H:\rust\100_days_rust\error_handling_demo\target\debug\error_handling_demo.exe: H:\rust\100_days_rust\error_handling_demo\src\main.rs
