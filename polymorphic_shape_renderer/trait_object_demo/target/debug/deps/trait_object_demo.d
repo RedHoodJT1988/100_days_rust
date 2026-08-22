592a09203e4d63ce3a4515fea610c582ecd98f72
Jonathan Reeves <jonathanedwardreeves1988@gmail.com>
2026-08-22 14:02:20 -0500

    Complete Day 33: Trait Object Demo (Dynamic Behavior Switching)

---
+++ polymorphic_shape_renderer/trait_object_demo/target/debug/deps/trait_object_demo.d
@@ -0,0 +1,5 @@
+H:\rust\100_days_rust\polymorphic_shape_renderer\trait_object_demo\target\debug\deps\trait_object_demo.d: src\main.rs
+
+H:\rust\100_days_rust\polymorphic_shape_renderer\trait_object_demo\target\debug\deps\trait_object_demo.exe: src\main.rs
+
+src\main.rs:
---
+++ polymorphic_shape_renderer/trait_object_demo/target/debug/trait_object_demo.d
@@ -0,0 +1,1 @@
+H:\rust\100_days_rust\polymorphic_shape_renderer\trait_object_demo\target\debug\trait_object_demo.exe: H:\rust\100_days_rust\polymorphic_shape_renderer\trait_object_demo\src\main.rs
